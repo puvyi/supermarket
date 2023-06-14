604bb75338346c3df1754adfff5e70d7c196652a
puvyi <puviy.m@gmail.com>
2023-06-14 17:40:28 +0200

    Classes updated

--- project/Customers.py
+++ project/Customers.py
@@ -1,45 +1,48 @@
 
 import random
+from transition import TRANSITION_MATRIX
+
 class Customer:
     """
     a single customer that moves through the supermarket
     in a MCMC simulation
     """
-    def __init__(self, name, state, transition_probs=1, budget=100):
+    def __init__(self, name, location, transition_probs=TRANSITION_MATRIX, budget=100):
         self.name = name
-        self.state = state
+        self.location = location
         self.budget = budget
-        self.path = []
+        self.path = [location]
         self.TM = transition_probs
+        self.active =True
 
     def __repr__(self):
-        print (f'<Customer {self.name} in {self.state}>')
+        return f'<Customer {self.name} in {self.location}>'
     
-    def next_state(self):
+    def move(self):
         '''
-        Propagates the customer to the next state.
+        Propagates the customer to the next location.
         Returns nothing.
         '''
-        self.state = random.choice(['spices', 'drinks', 'fruit', 'dairy'])
-
-    def is_active(self):
-        '''
-        Returns True if the customer has not reached the checkout yet.
-        '''
-        return self.state
-    
-
+        if self.active:
+            self.location = random.choice(['spices', 'drinks', 'fruit', 'dairy', 'checkout'], p=self.TM[self.location])
+            self.path.append(self.location)
+            if (self.location == 'checkout'):
+                 self.active=False
+                 print(f'Customer {self.name} is checking out')
 
 
 if __name__=='__main__':
-    c1 = Customer('Pradnya', 'spices')
-    c2 = Customer('Santiago', 'drinks')
-    c3 = Customer('Puviy', 'dairy')
+    c1 = Customer('Pradnya', location='spices')
+    '''c2 = Customer('Santiago', location='drinks')
+    c3 = Customer('Puviy', location='dairy')'''
     
-    customer_list = [c1,c2,c3]
-    for customer in customer_list:
+    #customer_list = [c1,c2,c3]
+    c1.__repr__()
+    c1.move()
+
+    '''for customer in customer_list:
         customer.__repr__()
         customer.__repr__()
-        customer.__repr__()
+        customer.__repr__()'''
     
 
--- project/Supermarket.py
+++ project/Supermarket.py
@@ -9,9 +9,31 @@ class Supermarket:
     def __init__(self, steps=10) -> None:
         self.customers = []
         self.steps =steps
+        self.time=0
+        self.active
+        self.activecustomers
+
+    def __repr__(self) -> str:
+        pass
 
     def add_customers(self, customer):
         self.customers.append(customer)
+
+    def add_customers(self, number):
+        for i in range(number):
+            self.add_customers(Customers())
+
+    def simulate():
+        pass
+
+    def move_all():
+        pass
+
+    def next_timestep():
+        time++
+        move_all
+        
+
 
 
     
